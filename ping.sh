c5b77e7b338c8b3581c6b397178849e9fb773cf1
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2023-03-11 15:26:56 -0800

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -3,5 +3,6 @@ export MAC=$(cat /sys/class/net/eth0/address)
 while true
 do
   wget --post-data ${MAC} 192.168.1.100:12345 -T 2 -q -O/dev/null
+  wget --post-data ${MAC} 192.168.1.110:12345 -T 2 -q -O/dev/null
   sleep 1
 done
