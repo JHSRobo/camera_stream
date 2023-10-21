308676f6f1240dfa004793f695a60065475616de
Alex Bertran <70992408+ApBertran@users.noreply.github.com>
2023-10-21 11:46:27 -0700

    camera 3 streaming test

--- stream.sh
+++ stream.sh
@@ -7,15 +7,16 @@ if [[ "$(id -u)" != 0 ]]
   exit
 fi
 
-grep -q "Setup" /etc/rc.local || ( echo "Please run setup!" && exit )
-raspi-config nonint do_legacy 0
+# grep -q "Setup" /etc/rc.local || ( echo "Please run setup!" && exit )
+# raspi-config nonint do_legacy 0
 
 # Start up camera streamer
 bash /home/jhsrobo/camera_stream/ping.sh &
 
-export ROTATION=0
-export WIDTH=1280
-export HEIGHT=720
-export FPS=45
+# export ROTATION=0
+# export WIDTH=1280
+# export HEIGHT=720
+# export FPS=45
 
-raspivid -n -cd MJPEG -awb auto -ifx none -b 25000000 -br 60 -t 0 -rot ${ROTATION} -w ${WIDTH} -h ${HEIGHT} -fps ${FPS} -o - | ncat -lkv4 5000
+# raspivid -n -cd MJPEG -awb auto -ifx none -b 25000000 -br 60 -t 0 -rot ${ROTATION} -w ${WIDTH} -h ${HEIGHT} -fps ${FPS} -o - | ncat -lkv4 5000
+libcamera-vid -t 10000 --codec mjpeg -o | ncat -lkv4 5000
