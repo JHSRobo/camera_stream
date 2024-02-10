646b9afb7c0cc468615aae0397ebc1e5e5cc6295
James-Randall-14 <jammerand14@gmail.com>
2024-02-10 19:19:01 +0000

    switched to H264 and UDP

--- stream.sh
+++ stream.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 # Script to be run at EVERY boot
 
@@ -8,7 +8,6 @@ if [[ "$(id -u)" != 0 ]]
 fi
 
 grep -q "Setup" /etc/rc.local || ( echo "Please run setup!" && exit )
-#raspi-config nonint do_legacy 0
 
 # Start up camera streamer
 bash /home/jhsrobo/camera_stream/ping.sh &
@@ -18,4 +17,7 @@ export WIDTH=1440
 export HEIGHT=810
 export FPS=50
 
-libcamera-vid --framerate ${FPS} --width ${WIDTH} --height ${HEIGHT} -t 0 --codec mjpeg --inline --listen -o - | ncat -lkv4 5000
+# Get the last 3 digits of the IP
+export PORT=5$(hostname -I | cut -f1 -d' ' | cut -c 11-13)
+
+libcamera-vid --framerate ${FPS} --width ${WIDTH} --height ${HEIGHT} -t 0 --codec mjpeg --inline --listen -o udp://192.168.1.100:${PORT}
