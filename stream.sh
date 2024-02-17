6b906396836b30c431858e567aab74fd859dd531
Clever-Student <fringsj26@student.jhs.net>
2024-02-17 10:28:45 -0800

    fixed an error

--- stream.sh
+++ stream.sh
@@ -13,10 +13,10 @@ bash /home/jhsrobo/camera_stream/ping.sh &
 #export ROTATION=0
 export WIDTH=1440
 export HEIGHT=810
-export FPS=50
+export FPS=30
 
 # Get the last 3 digits of the IP
 export PORT=5$(hostname -I | cut -f1 -d' ' | cut -c 11-13)
 
-libcamera-vid --framerate ${FPS} --width ${WIDTH} --height ${HEIGHT} -t 0 --inline 1 -g 1 --listen -o udp://192.168.1.100:5${PORT}
+libcamera-vid --framerate ${FPS} --width ${WIDTH} --height ${HEIGHT} --inline 1 -g 1 -t 0 --listen -o udp://192.168.1.100:${PORT}
 
