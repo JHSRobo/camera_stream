e76088b1cb4e315c95d122b5255638a27dabf5cc
jack-frings <jackdfrings@protonmail.com>
2025-11-14 19:02:17 -0800

    removed old ustreamer install file that's now in core

--- ustreamer_install.bash
+++
@@ -1,6 +0,0 @@
-apt install build-essential libevent-dev libjpeg-dev libbsd-dev
-cd /home/jhsrobo
-git clone --depth=1 https://github.com/pikvm/ustreamer
-cd ustreamer
-make
-cp ustreamer /usr/local/bin
